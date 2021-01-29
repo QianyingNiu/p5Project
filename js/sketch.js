c124ac62aa7c76a7913813fa886e44f5fa0aae3b
QianyingNiu <qniu@saic.edu>
2021-01-29 08:50:48 -0600

    Commiting p5 files and index files 画图形/颜色描边/鼠标轨迹追踪

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,48 @@
 
 
 function setup() {
-  // put setup code here to run once
+  // put setup code here to run once固定代码
 
-  // create canvas 500 by 500 px
-  createcanvas(500, 500);
+  // create canvas 500 by 500 px创建画布
+  createCanvas(windowWidth, windowHeight);
 
-  background(0);
+  background(255, 204, 0);
+
+  //print(windowWidth);查看网页宽
+
+  //change rectangle x,y coordinates to center of rect矩形中心
+  rectMode(CENTER);
 }
 
 function draw() {
-  // put drawing code here to run in a loop
+  smooth();
+
+  //styles for the ellipse圆形颜色和描边
+  noStroke();
+  // put drawing code here to run in a loop循环代码/画形状
+  ellipse(mouseX, mouseX, mouseY, 150);
+
+  //styles for the rect矩形颜色和描边
+  stroke(100, 255, 80);
+  strokeWeight(20);
+  fill(50, 200, 120);
+  rect(800, 400, 200, 300);
+
+  //styles for the triangle三角形颜色和描边
+  stroke(40, 200, 100);
+  strokeWeight(20);
+  fill(50, 30, 120);
+  triangle(1200, 50, 1300, 600, 950, 400);
+
+  noFill();
+  triangle(300, 400, 100, 400, mouseX, mouseY);
+
+  //styles for the point鼠标轨迹实时跟踪
+  fill(120);
+  stroke(40, 280, 100);
+  strokeWeight(10);
+  //point is only x, y coordinates
+  point(mouseX, mouseY);
+  print(mouseX);
+
 }
