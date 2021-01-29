031debdf2bfd8d3374d00e8f3567e4e856ea759f
QianyingNiu <qniu@saic.edu>
2021-01-29 06:27:50 -0600

    add background color

---
+++ js/sketch.js
@@ -0,0 +1,14 @@
+
+
+function setup() {
+  // put setup code here to run once
+
+  // create canvas 500 by 500 px
+  createcanvas(500, 500);
+
+  background(0);
+}
+
+function draw() {
+  // put drawing code here to run in a loop
+}
